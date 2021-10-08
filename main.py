c959d7ffbbad9cbd19b9e6152765b15698275275
Brandt <brandtd77@gmail.com>
2021-10-08 17:16:09 -0600

    Added 'if name main' clause

--- main.py
+++ main.py
@@ -9,22 +9,26 @@ import log_writer
 import visualize_data
 
 
-# INITIALIZE OBJECTS
-extract = extract_data.Extract_data()
-transform = transform_data.Transform_data()
-load = load_data.Load_data()
+def main():
+    # INITIALIZE OBJECTS
+    extract = extract_data.Extract_data()
+    transform = transform_data.Transform_data()
+    load = load_data.Load_data()
 
-logger = log_writer.Log_writer()
-visualize = visualize_data.Visualize_data()
+    logger = log_writer.Log_writer()
+    visualize = visualize_data.Visualize_data()
 
-# PIPELINE OPERATION
-# ==================
+    # PIPELINE OPERATION
+    # ==================
 
-# Retrieve files if they are outdated, log the extraction 
-file_list = extract.retrieve_ab_csv_files()
-logger.log_data_extraction(file_list)
+    # Retrieve files if they are outdated, log the extraction 
+    file_list = extract.retrieve_ab_csv_files()
+    logger.log_data_extraction(file_list)
 
-ab_covid_data = extract.get_alberta_covid_stats_dataframe()
-ab_covid_deaths = transform.get_deaths_by_age(ab_covid_data)
-visualize.create_bar_graph_of_data(ab_covid_deaths)
+    ab_covid_data = extract.get_alberta_covid_stats_dataframe()
+    ab_covid_deaths = transform.get_deaths_by_age(ab_covid_data)
+    visualize.create_bar_graph_of_data(ab_covid_deaths)
 
+
+if __name__ == '__main__':
+    main()
